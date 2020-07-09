e0894ccc46720990929449082d19e5539e419b3a
Richard Cizovsky <richard@neotreks.com>
2020-07-09 10:02:56 +0200

    creating only one image(replacing original image always) and added capture.php for creating and showing this one image

---
+++ www/capture.php
@@ -0,0 +1,10 @@
+<?php
+   define('BASE_DIR', dirname(__FILE__));
+   require_once(BASE_DIR.'/config.php');
+   $pipe = fopen("FIFO","w");
+   fwrite($pipe, "im,1" . "\n");
+   fclose($pipe);
+   usleep(500000);
+   header("Content-Type: image/jpeg");
+   readfile("/var/www/html/media/im.jpg");
+?>
