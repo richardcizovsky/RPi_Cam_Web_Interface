f970b0b0eccac99f44756c67f54814b2400003cd
Richard Cizovsky <richard@neotreks.com>
2020-08-24 13:23:50 +0200

    Added parametr which is used in usleep for example http://192.168.43.180/html/capture.php?capturetime=100000

--- www/capture.php
+++ www/capture.php
@@ -4,7 +4,11 @@
    $pipe = fopen("FIFO","w");
    fwrite($pipe, "im,1" . "\n");
    fclose($pipe);
-   usleep(1000000);
+   if (isset($_GET['capturetime'])) {
+    usleep($_GET['capturetime']);
+   } else {
+    usleep(1000000);
+   }
    header("Content-Type: image/jpeg");
    readfile("/var/www/html/media/im.jpg");
 ?>
